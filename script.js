9c60c87dbb6bbabdb690d0d757b2c6ed630c73d1
NicholasFoti <nickfoti@hotmail.com>
2024-03-05 18:14:27 +0800

    Changes img, added scroller, sizing adjustments

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function togglemenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const scrollers = document.querySelectorAll(".scroller");
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach(scroller => {
+        scroller.setAttribute('data-animated', true)
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
